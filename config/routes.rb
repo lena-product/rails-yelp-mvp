e51ecd899bb5d48f0637f3bc7c3b25d8f0e1bbb4
Lena Wessling Teicke <lena.wessling1@gmail.com>
2022-11-23 16:24:12 +0100

    solved all tasks only pretty frontend missing

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,13 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  # resources :restaurants, only: [:index, :new, :create, :show] do
+  #   resources :reviews, only: [:new, :create]
+  # end
+  get "restaurants", to: "restaurants#index"
+  get "restaurants/new", to: "restaurants#new", as: :new_restaurant
+  post "restaurants", to: "restaurants#create"
+  get "restaurants/:id", to: "restaurants#show", as: :restaurant
+  get "restaurants/:restaurant_id/reviews/new", to: "reviews#new", as: :new_restaurant_review
+  post "restaurants/:restaurant_id/reviews", to: "reviews#create", as: :restaurant_reviews
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+dishoom = {name: "HongKong", address: "7 Boundary St, London E2 7JE", category: "chinese"}
+pizza_east =  {name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", category: "italian"}
+crepes = {name: "Le Wagon Crepes", address: "56A Eiffel Tower St, Paris E1 6PQ", category: "french"}
+sushi = {name: "Sushi Express", address: "56A Shoreditch High St, Kioto E1 6PQ", category: "japanese"}
+frikandel = {name: "Frikandel", address: "56A Shoreditch High St, Brussels E1 6PQ", category: "belgian"}
+
+[dishoom, pizza_east, crepes, sushi, frikandel].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
